fe5badbff731f9447bae2551fcb5303442f4f9d7
Shunya Koide <56504519+shunyagithub@users.noreply.github.com>
2022-04-17 12:37:19 +0900

    add loading

--- src/Overlay.js
+++ src/Overlay.js
@@ -1,10 +1,16 @@
+import { useProgress } from '@react-three/drei';
 import React from 'react';
 
 export default function Overlay({ ready, clicked, setClicked }) {
+  const { progress } = useProgress();
+  const progressPercentage = progress.toFixed(1);
+
   return (
     <>
       <div className={`fullscreen bg ${ready ? 'ready' : 'notready'} ${clicked && 'clicked'}`}>
-        <div onClick={() => ready && setClicked(true)}>{!ready ? 'Loading...' : 'Click to continue'}</div>
+        <div onClick={() => ready && setClicked(true)}>
+          {!ready ? `Loading ${progressPercentage}%` : 'Click to continue'}
+        </div>
       </div>
     </>
   );
